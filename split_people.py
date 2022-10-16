f6235d2c54a34622b63b8f1b78b68d74cc108e61
the real gabe guo <gzg2104@columbia.edu>
2022-10-16 13:56:17 +0000

    added more documentation

--- split_people.py
+++ split_people.py
@@ -1,16 +1,46 @@
 """
 
 This script does the train-test-val split.
-It assumes that all the items were already in the train folder, and arranged in subfolders by class name.
+It assumes that all the items were already in the train folder, 
+    and arranged in subfolders by class name.
+    If all items weren't in train folder already, it moves them all there.
+Keeps some classes in training folder, 
+    moves some to val folder, 
+    moves some to test folder.
+We have disjoint classes in train, val, test; 
+    as we are doing siamese neural networks to find fingerprint similarity.
+
 Ex:
+Start State:
     data_folder
         /train
             /class1
-                sample.jpg
-                another_sample.jpg
+                class1sample1.jpg
+                class1sample2.jpg
             /class2
+                class2sample1.jpg
+                class2sample2.jpg
+            /class3
+                class3sample1.jpg
+                class3sample2.jpg
+
+End State:
+    data_folder
+        /train
+            /class 1
+                class1sample1.jpg
+                class1sample2.jpg
+        /val
+            /class2
+                class2sample1.jpg
+                class2sample2.jpg
+        /test
+            /class3
+                class3sample1.jpg
+                class3sample2.jpg
 
 """
 
 if __name__ == "__main__":
-    # Usage: 
+    usage_msg = "Usage: split_people.py --train_percent <(0, 1)> --val_percent <(0, 1)>" + \
+            "\nNote: train_percent + val_percent < 1, test_percent = 1 - (train_percent + val_percent)"
