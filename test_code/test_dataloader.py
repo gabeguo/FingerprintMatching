822c6ef06d5740196ca952d31370cf34517ad7ed
Gabe <gzg2104@columbia.edu>
2022-10-19 21:58:14 -0400

    fixed order of imports

--- test_code/test_dataloader.py
+++ test_code/test_dataloader.py
@@ -1,13 +1,14 @@
 import torch
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
-from fingerprint_dataset import SiameseFingerprintDataset
-import common_filepaths
 import sys
 import os
 
 sys.path.insert(1, '../dl_models')
 sys.path.insert(1, '../')
+
+from fingerprint_dataset import SiameseFingerprintDataset
+import common_filepaths
 
 training_data = SiameseFingerprintDataset(os.path.join(DATA_FOLDER, 'train'))
 train_dataloader = DataLoader(training_data, batch_size=1, shuffle=True)
