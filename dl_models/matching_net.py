28dd5c7df9b72f3907f81988733e63515cf62e66
Gabe <gzg2104@columbia.edu>
2022-10-25 14:15:10 -0400

    use resnet as feature embedder

--- dl_models/matching_net.py
+++ dl_models/matching_net.py
@@ -1,22 +1,64 @@
+# Thanks https://github.com/adambielski/siamese-triplet
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import models
 
-class MatchingNet(nn.Module):
-    def __init__(self, embedding_len = 1000):
-        self.feature_extractor = models.resnet18(pretrained=True)
-        num_features = self.feature_extractor.in_features
-        self.feature_extractor.fc = nn.Linear(num_features, embedding_len)
-        self.distance = torch.cdist
-        self.final_activation = nn.Sigmoid()
-        return
+class EmbeddingNet(nn.Module):
+    def __init__(self):
+        super(EmbeddingNet, self).__init__()
+        # Thanks # https://discuss.pytorch.org/t/how-can-l-use-the-pre-trained-resnet-to-extract-feautres-from-my-own-dataset/9008
+        self.the_resnet = models.resnet18(pretrained=True)
+        modules=list(resnet18.children())[:-1]
+        self.feature_extractor=nn.Sequential(*modules)
+        self.fc = the_resnet.fc
+
+    def forward(self, x):
+        output = self.feature_extractor(x)
+        print(output.shape)
+        return output
+
+    def get_embedding(self, x):
+        return self.forward(x)
+
+
+class EmbeddingNetL2(EmbeddingNet):
+    def __init__(self):
+        super(EmbeddingNetL2, self).__init__()
+
+    def forward(self, x):
+        output = super(EmbeddingNetL2, self).forward(x)
+        output /= output.pow(2).sum(1, keepdim=True).sqrt()
+        return output
+
+    def get_embedding(self, x):
+        return self.forward(x)
+
+class SiameseNet(nn.Module):
+    def __init__(self, embedding_net):
+        super(SiameseNet, self).__init__()
+        self.embedding_net = embedding_net
 
     def forward(self, x1, x2):
-        output1 = self.feature_extractor(x1)
-        output2 = self.feature_extractor(x2)
-        distance = self.distance(x1, x2, p=2)
-        prob_same_class = self.final_activation(distance)
-        return prob_same_class
+        output1 = self.embedding_net(x1)
+        output2 = self.embedding_net(x2)
+        return output1, output2
 
-    # will use softplus loss
+    def get_embedding(self, x):
+        return self.embedding_net(x)
+
+
+class TripletNet(nn.Module):
+    def __init__(self, embedding_net):
+        super(TripletNet, self).__init__()
+        self.embedding_net = embedding_net
+
+    def forward(self, x1, x2, x3):
+        output1 = self.embedding_net(x1)
+        output2 = self.embedding_net(x2)
+        output3 = self.embedding_net(x3)
+        return output1, output2, output3
+
+    def get_embedding(self, x):
+        return self.embedding_net(x)
