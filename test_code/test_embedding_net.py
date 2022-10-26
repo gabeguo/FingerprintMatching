b747eec45c8e7acff506452fcf22531f86c5ebbd
the real gabe guo <gzg2104@columbia.edu>
2022-10-26 18:29:41 +0000

    updated test code for embedding models

--- test_code/test_embedding_net.py
+++ test_code/test_embedding_net.py
@@ -10,7 +10,7 @@ sys.path.append('../')
 
 from fingerprint_dataset import FingerprintDataset
 from siamese_datasets import SiameseDataset, TripletDataset
-from embedding_models import EmbeddingNet
+from embedding_models import EmbeddingNet, SiameseNet, TripletNet
 from common_filepaths import DATA_FOLDER
 
 print('pair loading test\n')
@@ -28,13 +28,39 @@ print(num_positive_examples)
 
 print('\ntriplet loading test\n')
 
+batch_size=4
+
 triplet_dataset = TripletDataset(FingerprintDataset(os.path.join(DATA_FOLDER, 'train'), train=True))
-triplet_dataloader = DataLoader(triplet_dataset, batch_size=8, shuffle=True)
+triplet_dataloader = DataLoader(triplet_dataset, batch_size=batch_size, shuffle=True)
 
 num_positive_examples = 0
-for i in range(30):#range(len(training_data)):
+
+triplet_net = TripletNet(EmbeddingNet())
+
+# distances between embedding of positive and negative pair
+_01_dist = []
+_02_dist = []
+
+dist = torch.nn.CosineSimilarity(dim=0, eps=1e-8)
+
+for i in range(10):#range(len(training_data)):
     test_images, test_labels, test_filepaths = next(iter(triplet_dataloader))
-    embedder = EmbeddingNet()
-    #for img in test_images:
-    #    print(embedder(img).size())
+    
+    embeddings = [torch.reshape(e, (batch_size, e.size()[1])) for e in triplet_net(*test_images)]
+    # embeddings.shape[0] is (anchor, pos, neg); embeddings.shape[1] is batch size; embeddings.shape[2] is embedding length
+    print([embedding.size() for embedding in embeddings])
+    
+    for batch_index in range(batch_size):
+        _01_dist.append(dist(embeddings[0][batch_index], embeddings[1][batch_index]).item())
+        _02_dist.append(dist(embeddings[0][batch_index], embeddings[2][batch_index]).item())
+
     print(test_filepaths, test_labels)
+
+#print(_01_dist[0].size())
+#print(_02_dist[0].size())
+
+print(len(_01_dist))
+print(len(_02_dist))
+
+print('average cosine sim between matching pairs:', sum(_01_dist) / len(_01_dist))
+print('average cosine sim between non-matching pairs:', sum(_02_dist) / len(_02_dist))
