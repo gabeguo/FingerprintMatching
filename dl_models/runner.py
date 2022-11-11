4ac768bac00d475fae55b0943d5a85c6b86c8132
the real gabe guo <gzg2104@columbia.edu>
2022-11-11 15:53:45 +0000

    changed test batch size again

--- dl_models/runner.py
+++ dl_models/runner.py
@@ -20,7 +20,7 @@ from common_filepaths import DATA_FOLDER
 MODEL_PATH = '/data/therealgabeguo/embedding_net_weights.pth'
 
 batch_size=16
-test_batch_size=2
+test_batch_size=4
 
 training_dataset = TripletDataset(FingerprintDataset(os.path.join(DATA_FOLDER, 'train'), train=True))
 #training_dataset = torch.utils.data.Subset(training_dataset, list(range(0, len(training_dataset), 50)))
