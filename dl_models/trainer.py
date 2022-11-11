cea397ef3db46f81daabdbdfde43377a6a5d75de
the real gabe guo <gzg2104@columbia.edu>
2022-11-11 15:46:58 +0000

    fixed bug in logging

--- dl_models/trainer.py
+++ dl_models/trainer.py
@@ -57,9 +57,10 @@ def fit(train_loader, val_loader, model, loss_fn, optimizer, scheduler, n_epochs
             from datetime import datetime
             datetime_str = datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
             with open('/data/therealgabeguo/results/train_res_{}.txt'.format(datetime_str), 'w') as fout:
-                fout.write('epoch: ' + str([epoch for epoch in range(start_epoch, n_epochs)]) + '\n')
+                fout.write('epoch: ' + str([epoch for epoch in range(start_epoch, epoch + 1)]) + '\n')
                 fout.write('train loss: ' + str(past_train_losses) + '\n')
                 fout.write('val loss: ' + str(past_val_losses) + '\n')
+                fout.write('stopped on epoch {}\n'.format(epoch))
 
             return best_val_epoch, best_val_loss
 
