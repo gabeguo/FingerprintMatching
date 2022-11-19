4fbb94e36c5968d8f4afb83888ba825e45cd1935
the real gabe guo <gzg2104@columbia.edu>
2022-11-19 20:07:09 +0000

    runner ready to run on the synthetic dataset

--- synthetic_data_transfer_learning/runner.py
+++ synthetic_data_transfer_learning/runner.py
@@ -17,21 +17,21 @@ from embedding_models import *
 
 from common_filepaths import DATA_FOLDER, SYNTHETIC_DATA_FOLDER
 
-MODEL_PATH = '/data/therealgabeguo/embedding_net_weights.pth'
+MODEL_PATH = '/data/therealgabeguo/embedding_net_weights_printsgan.pth'
 
 batch_size=64
-test_batch_size=64
+test_batch_size=4
 
 training_dataset = TripletDataset(FingerprintDataset(os.path.join(SYNTHETIC_DATA_FOLDER, 'train'), train=True))
 #training_dataset = torch.utils.data.Subset(training_dataset, list(range(0, len(training_dataset), 50)))
 train_dataloader = DataLoader(training_dataset, batch_size=batch_size, shuffle=True)
 
 val_dataset = TripletDataset(FingerprintDataset(os.path.join(SYNTHETIC_DATA_FOLDER, 'val'), train=False))
-#val_dataset = torch.utils.data.Subset(val_dataset, list(range(0, len(val_dataset), 5)))
+#val_dataset = torch.utils.data.Subset(val_dataset, list(range(0, len(val_dataset), 50)))
 val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
 
 test_dataset = TripletDataset(FingerprintDataset(os.path.join(SYNTHETIC_DATA_FOLDER, 'test'), train=False))
-#test_dataset = torch.utils.data.Subset(test_dataset, list(range(0, len(test_dataset), 5)))
+#test_dataset = torch.utils.data.Subset(test_dataset, list(range(0, len(test_dataset), 50)))
 test_dataloader = DataLoader(test_dataset, batch_size=test_batch_size, shuffle=True)
 
 # SHOW IMAGES
@@ -98,7 +98,7 @@ best_val_epoch, best_val_loss = 0, 0
 
 best_val_epoch, best_val_loss = fit(train_loader=train_dataloader, val_loader=val_dataloader, model=triplet_net, \
     loss_fn=nn.TripletMarginLoss(margin=tripletLoss_margin), optimizer=optimizer, scheduler=scheduler, \
-    n_epochs=50, cuda='cuda:1', log_interval=100, metrics=[], start_epoch=0, early_stopping_interval=20)
+    n_epochs=50, cuda='cuda:1', log_interval=50, metrics=[], start_epoch=0, early_stopping_interval=15)
 
 log += 'best_val_epoch = {}\nbest_val_loss = {}\n'.format(best_val_epoch, best_val_loss)
 print('best_val_epoch = {}\nbest_val_loss = {}\n'.format(best_val_epoch, best_val_loss))
