e9159f607e015c7943f2ed251d2076f16696abef
the real gabe guo <gzg2104@columbia.edu>
2022-11-23 20:48:12 +0000

    added subset data folder to common filepaths

--- common_filepaths.py
+++ common_filepaths.py
@@ -1,2 +1,3 @@
 DATA_FOLDER = '/data/therealgabeguo/fingerprint_data/sd302_split'
 SYNTHETIC_DATA_FOLDER = '/data/therealgabeguo/printsgan'
+SUBSET_DATA_FOLDER = '/data/therealgabeguo/fingerprint_data/sd302_split_subset_R_S_U_V_J_N_Q'
