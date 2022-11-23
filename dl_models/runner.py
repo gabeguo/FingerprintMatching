fb33e38cab86e2e8a0291b0a6fe8517b814e8e71
the real gabe guo <gzg2104@columbia.edu>
2022-11-23 20:52:39 +0000

    improved logging

--- dl_models/runner.py
+++ dl_models/runner.py
@@ -15,7 +15,7 @@ from siamese_datasets import *
 from fingerprint_dataset import *
 from embedding_models import *
 
-from common_filepaths import DATA_FOLDER
+from common_filepaths import DATA_FOLDER, SUBSET_DATA_FOLDER
 
 PRETRAINED_MODEL_PATH = '/data/therealgabeguo/embedding_net_weights_printsgan.pth'
 POSTRAINED_MODEL_PATH = '/data/therealgabeguo/embedding_net_weights.pth'
@@ -23,15 +23,15 @@ POSTRAINED_MODEL_PATH = '/data/therealgabeguo/embedding_net_weights.pth'
 batch_size=64
 test_batch_size=16
 
-training_dataset = TripletDataset(FingerprintDataset(os.path.join(DATA_FOLDER, 'train'), train=True))
+training_dataset = TripletDataset(FingerprintDataset(os.path.join(SUBSET_DATA_FOLDER, 'train'), train=True))
 #training_dataset = torch.utils.data.Subset(training_dataset, list(range(0, len(training_dataset), 50)))
 train_dataloader = DataLoader(training_dataset, batch_size=batch_size, shuffle=True)
 
-val_dataset = TripletDataset(FingerprintDataset(os.path.join(DATA_FOLDER, 'val'), train=False))
+val_dataset = TripletDataset(FingerprintDataset(os.path.join(SUBSET_DATA_FOLDER, 'val'), train=False))
 #val_dataset = torch.utils.data.Subset(val_dataset, list(range(0, len(val_dataset), 5)))
 val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
 
-test_dataset = TripletDataset(FingerprintDataset(os.path.join(DATA_FOLDER, 'test'), train=False))
+test_dataset = TripletDataset(FingerprintDataset(os.path.join(SUBSET_DATA_FOLDER, 'test'), train=False))
 #test_dataset = torch.utils.data.Subset(test_dataset, list(range(0, len(test_dataset), 5)))
 test_dataloader = DataLoader(test_dataset, batch_size=test_batch_size, shuffle=True)
 
@@ -65,11 +65,17 @@ log = ""
 # CREATE EMBEDDER
 pretrained=False # on image net
 embedder = EmbeddingNet(pretrained=pretrained)
-log += 'pretrained: {}\n'.format(pretrained)
-print('pretrained:', pretrained)
+log += 'pretrained on image net: {}\n'.format(pretrained)
+print('pretrained on image net:', pretrained)
 
 # load saved weights!
-embedder.load_state_dict(torch.load(PRETRAINED_MODEL_PATH))
+pretrained_other_data = True
+if pretrained_other_data:
+    embedder.load_state_dict(torch.load(PRETRAINED_MODEL_PATH))
+
+pretrained_other_msg = 'pretrained on other data: {}\n'.format(pretrained_other_data)
+print(pretraied_other_msg)
+log += pretrained_other_msg
 
 """
 # freeze all layers except the last one
@@ -99,7 +105,7 @@ best_val_epoch, best_val_loss = 0, 0
 
 best_val_epoch, best_val_loss = fit(train_loader=train_dataloader, val_loader=val_dataloader, model=triplet_net, \
     loss_fn=nn.TripletMarginLoss(margin=tripletLoss_margin), optimizer=optimizer, scheduler=scheduler, \
-    n_epochs=50, cuda='cuda:1', log_interval=100, metrics=[], start_epoch=0, early_stopping_interval=15)
+    n_epochs=50, cuda='cuda:1', log_interval=100, metrics=[], start_epoch=0, early_stopping_interval=25)
 
 log += 'best_val_epoch = {}\nbest_val_loss = {}\n'.format(best_val_epoch, best_val_loss)
 print('best_val_epoch = {}\nbest_val_loss = {}\n'.format(best_val_epoch, best_val_loss))
