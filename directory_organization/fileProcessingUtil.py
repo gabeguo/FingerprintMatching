b2d934408e263d09fb7b7af8b19db8ea9cee6b15
the real gabe guo <gzg2104@columbia.edu>
2022-12-31 20:37:21 +0000

    addded check in file processing that we only have filename, not full path

--- directory_organization/fileProcessingUtil.py
+++ directory_organization/fileProcessingUtil.py
@@ -1,14 +1,17 @@
 def get_id(filename):
+    assert '/' not in filename
     #SUBJECT_ENCOUNTER_DEVICE_CAPTURE_RESOLUTION_FGRP.EXT (SD301) or
     #SUBJECT_DEVICE_RESOLUTION_CAPTURE_FRGP.EXT (SD302)
     return filename.split('_')[0]
 
 def get_fgrp(filename):
+    assert '/' not in filename
     filename = filename[:-4]
     return filename.split('_')[-1]
     #return filename.replace('.', '_').split('_')[-2]
 
 def get_sensor(filename):
+    assert '/' not in filename
     tokens = filename.split('_')
     # SD301 version
     if tokens[1].isnumeric():
