5ad776c72148dad05c8f5366bda9781b0a6f1a9b
the real gabe guo <gzg2104@columbia.edu>
2023-01-04 20:31:30 +0000

    correct formatting of dates

--- synthetic_data_transfer_learning/trainer.py
+++ synthetic_data_transfer_learning/trainer.py
@@ -56,7 +56,7 @@ def fit(train_loader, val_loader, model, loss_fn, optimizer, scheduler, n_epochs
             model.eval()
             
             from datetime import datetime
-            datetime_str = datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
+            datetime_str = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
             with open('/data/therealgabeguo/pretrain_results/train_res_{}.txt'.format(datetime_str), 'w') as fout:
                 fout.write('epoch: ' + str([epoch for epoch in range(start_epoch, epoch + 1)]) + '\n')
                 fout.write('train loss: ' + str(past_train_losses) + '\n')
@@ -76,7 +76,7 @@ def fit(train_loader, val_loader, model, loss_fn, optimizer, scheduler, n_epochs
             scheduler.step()
 
     from datetime import datetime
-    datetime_str = datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
+    datetime_str = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
     with open('/data/therealgabeguo/pretrain_results/train_res_{}.txt'.format(datetime_str), 'w') as fout:
         fout.write('epoch: ' + str([epoch for epoch in range(start_epoch, n_epochs)]) + '\n')
         fout.write('train loss: ' + str(past_train_losses) + '\n')
