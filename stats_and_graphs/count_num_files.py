535b34c5ea8334b50d530a18b5cfc26af6bfa48f
the real gabe guo <gzg2104@columbia.edu>
2023-01-27 22:40:24 +0000

    option to count high res duplicates or not

--- stats_and_graphs/count_num_files.py
+++ stats_and_graphs/count_num_files.py
@@ -2,10 +2,11 @@ import os
 import argparse
 
 # counts number of fingerprint images, excluding duplicates
-def main(data_folder):
+def main(data_folder, count_high_res=False):
     n_files = sum([len([the_file for the_file in files \
-        if ('.png' in the_file or '.jpg' in the_file) \
-        and '_1000_' not in the_file and '_2000_' not in the_file]) \
+        if ('.png' in the_file or '.jpg' in the_file or '.bmp' in the_file) \
+        and (count_high_res or ('_1000_' not in the_file and '_2000_' not in the_file)) \
+        ]) \
         for r, d, files in os.walk(data_folder)])
     n_classes = len(os.listdir(data_folder))
 
@@ -15,7 +16,9 @@ def main(data_folder):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser('count_num_files.py')
     parser.add_argument('--dataset', '-d', help='Path to folders containing images', type=str)
+    parser.add_argument('--high_res', '-r', help='Count duplicate high res (1000, 2000 px) images', action='store_true')
     args = parser.parse_args()
     dataset = args.dataset
+    high_res = args.high_res
 
-    main(dataset)
+    main(dataset, high_res)
