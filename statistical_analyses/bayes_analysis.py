c6ba04c3101e3f3f34da96990260a04b85ae53ae
Aniv Ray <anivray10@gmail.com>
2023-02-16 20:47:07 +0000

    Added current leads tester

--- statistical_analyses/bayes_analysis.py
+++ statistical_analyses/bayes_analysis.py
@@ -4,15 +4,16 @@ def geometric_distribution(fp, fn, prior, alpha):
     A = ( (1 - fn) * prior ) / ( (1 - fn) * prior + fp * (1 - prior) )
     B = 1 - A
     print("Probability of same person given a positive test = ", A)
-    i = 1
-    while True:
-        prob = geom.cdf(i, A)
-        if i % 10 == 0:
-            print("Probability with ", i, " samples = ", prob)
-        if prob > alpha:
-            print("It will take ", i - 1, "false positives to get the first true positive with ", alpha * 100, "% certainty")
-            return i
-        i += 1
+    return 1 / A
+    # i = 1
+    # while True:
+    #     prob = geom.cdf(i, A)
+    #     if i % 10 == 0:
+    #         print("Probability with ", i, " samples = ", prob)
+    #     if prob > alpha:
+    #         print("It will take ", i - 1, "false positives to get the first true positive with ", alpha * 100, "% certainty")
+    #         return i
+    #     i += 1
     #print("It will take more than ", i, " false positives to get the first true positive with ", alpha * 100, "% certainty")
     #return i
 
