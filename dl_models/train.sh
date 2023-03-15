63b1eed214735974f464b42c1ea5905abd4a2895
the real gabe guo <gzg2104@columbia.edu>
2023-03-15 01:55:44 +0000

    updated training shell script

--- dl_models/train.sh
+++ dl_models/train.sh
@@ -1,5 +1,9 @@
 # Prelim two-finger experiment
 python3 parameterized_runner.py \
-    --datasets '/data/therealgabeguo/fingerprint_data/sd302_split /data/therealgabeguo/fingerprint_data/sd300a_split /data/therealgabeguo/fingerprint_data/RidgeBase_Split' \
+    --datasets '/data/therealgabeguo/fingerprint_data/sd302_split /data/therealgabeguo/fingerprint_data/RidgeBase_Split' \
     --possible-fgrps '02 03' \
-    --posttrained-model-path /data/therealgabeguo/fingerprint_weights/dual_02_03_model.pth
+    --posttrained-model-path /data/therealgabeguo/fingerprint_weights/dual_02_03_model.pth \
+    --diff-fingers-across-sets-train \
+    '--diff-sensors-across-sets-train \
+    --diff-fingers-across-sets-val \
+    '--diff-sensors-across-sets-val
