ed1e05996cd1b819eb3475ce3e10a5da66ee6008
the real gabe guo <gzg2104@columbia.edu>
2023-07-08 21:28:46 +0000

    started run all experiments script

--- run_all_experiments.sh
+++ run_all_experiments.sh
@@ -1,6 +1,13 @@
+# variables
+root_folder='/data/therealgabeguo/_reproduced_fingerprint_results'
+cuda=2
+
+## Pretrain on PrintsGAN
 cd synthetic_data_transfer_learning
-CUDA_VISIBLE_DEVICES=2 python3 runner.py \
-    --model_path '/data/therealgabeguo/embedding_net_weights_printsgan.pth' \
-    --data_path '/data/therealgabeguo/printsgan' \
-    --output_folder '/data/therealgabeguo/pretrain_results'
+CUDA_VISIBLE_DEVICES=$cuda python3 runner.py \
+    --model_path "{$root_folder}/model_weights/embedding_net_weights_printsgan.pth" \
+    --data_path "/data/therealgabeguo/printsgan" \
+    --output_folder "{$root_folder}/pretrain_results"
 cd ../
+
+## Regular train
