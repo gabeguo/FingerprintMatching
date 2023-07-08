69604d8f14e5c7a6737ab1f2d09fcdbc20c9126b
the real gabe guo <gzg2104@columbia.edu>
2023-07-08 21:37:51 +0000

    fixed typo in model path creation

--- run_all_experiments.sh
+++ run_all_experiments.sh
@@ -5,9 +5,9 @@ cuda=2
 ## Pretrain on PrintsGAN
 cd synthetic_data_transfer_learning
 CUDA_VISIBLE_DEVICES=$cuda python3 runner.py \
-    --model_path "{$root_folder}/model_weights/embedding_net_weights_printsgan.pth" \
+    --model_path "${root_folder}/model_weights/embedding_net_weights_printsgan.pth" \
     --data_path "/data/therealgabeguo/printsgan" \
-    --output_folder "{$root_folder}/pretrain_results"
+    --output_folder "${root_folder}/pretrain_results"
 cd ../
 
 ## Regular train
