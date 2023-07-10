505ae6997f24819084d73ef5f1a5b83dd1e1aa3b
the real gabe guo <gzg2104@columbia.edu>
2023-07-10 01:00:04 +0000

    made CUDA a passable parameter to run_all_experiments.sh

--- run_all_experiments.sh
+++ run_all_experiments.sh
@@ -1,18 +1,19 @@
+# sh run_all_experiments.sh cuda
+
 # variables
 root_folder='/data/therealgabeguo/_reproduced_fingerprint_results'
-cuda=2
 
-## Pretrain on PrintsGAN
-cd synthetic_data_transfer_learning
-CUDA_VISIBLE_DEVICES=$cuda python3 runner.py \
-    --model_path "${root_folder}/model_weights/embedding_net_weights_printsgan.pth" \
-    --data_path "/data/therealgabeguo/printsgan" \
-    --output_folder "${root_folder}/pretrain_results"
-cd ../
+# ## Pretrain on PrintsGAN
+# cd synthetic_data_transfer_learning
+# CUDA_VISIBLE_DEVICES=$1 python3 runner.py \
+#     --model_path "${root_folder}/model_weights/embedding_net_weights_printsgan.pth" \
+#     --data_path "/data/therealgabeguo/printsgan" \
+#     --output_folder "${root_folder}/pretrain_results"
+# cd ../
 
 ## Regular train
 cd dl-models
-sh train.sh $root_folder $cuda
+sh train.sh $root_folder $1
 
 ## Test
-sh run_tests.sh $root_folder $cuda
+sh run_tests.sh $root_folder $1
