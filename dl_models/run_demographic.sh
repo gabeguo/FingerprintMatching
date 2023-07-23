2d5c097f7cbd2bc4923b10757f0031f26ce8fecc
the real gabe guo <gzg2104@columbia.edu>
2023-07-23 00:55:36 +0000

    have code for re-shuffling the demographic groups and training and testing, all in one go (automatic) - no data leakage

--- dl_models/run_demographic.sh
+++ dl_models/run_demographic.sh
@@ -2,76 +2,100 @@
 
 FAIRNESS_OUTPUT_FOLDER=$1/paper_results/fairness
 
-DEMOGRAPHICS_ROOT="/data/verifiedanivray/demographics_datasets"
+DEMOGRAPHICS_ROOT="/data/therealgabeguo/demographics_datasets"
 CAUCASIAN_DESCENT="sd302_white_split"
 NON_CAUCASIAN="sd302_non-white_split"
-COMBINED_RACE="sd302_white_and_non-white_split"
 MALE_GROUP="sd302_male_split"
 FEMALE_GROUP="sd302_female_split"
-COMBINED_GENDER="sd302_male_and_female_split"
 
-######
-# Training demographic models
-######
-#for category in $CAUCASIAN_DESCENT $NON_CAUCASIAN $COMBINED_RACE $MALE_GROUP $FEMALE_GROUP $COMBINED_GENDER
-for category in $COMBINED_RACE $COMBINED_GENDER
+for i in {0..9}
 do
-    folder="${DEMOGRAPHICS_ROOT}/${category}"
+    cd ../directory_organization
+    mkdir output
+    for subdir in $CAUCASIAN_DESCENT $NON_CAUCASIAN $MALE_GROUP $FEMALE_GROUP
+    do
+        python3 split_people.py --data_folder "${DEMOGRAPHICS_ROOT}/${subdir}" \
+            --train_percent 80 --val_percent 10 --rotate 10 >> output/splits.txt
+    done
+
+    cd ../dl_models
+
+    ######
+    # Training demographic models
+    ######
+    # Train individual demographics
+    for category in $CAUCASIAN_DESCENT $NON_CAUCASIAN $MALE_GROUP $FEMALE_GROUP
+    do
+        folder="${DEMOGRAPHICS_ROOT}/${category}"
+        CUDA_VISIBLE_DEVICES=$2 python3 parameterized_runner.py \
+            --datasets "${folder}" \
+            --val-datasets "${folder}" \
+            --posttrained-model-path $1/model_weights/demographic_model_${category}.pth \
+            --temp_model_dir 'temp_weights' --results_dir "$1/results" \
+            --diff-fingers-across-sets-train --diff-sensors-across-sets-train --diff-fingers-across-sets-val --diff-sensors-across-sets-val \
+            --scale-factor 2 --log-interval 100
+    done
+    # Also train combined demographics
+    folders="${DEMOGRAPHICS_ROOT}/${CAUCASIAN_DESCENT} ${DEMOGRAPHICS_ROOT}/${NON_CAUCASIAN}"
     CUDA_VISIBLE_DEVICES=$2 python3 parameterized_runner.py \
-        --datasets "${folder}" \
-        --val-datasets "${folder}" \
-        --posttrained-model-path $1/model_weights/demographic_model_${category}.pth \
+        --datasets "${folders}" \
+        --val-datasets "${folders}" \
+        --posttrained-model-path $1/model_weights/demographic_model_combined_race.pth \
         --temp_model_dir 'temp_weights' --results_dir "$1/results" \
         --diff-fingers-across-sets-train --diff-sensors-across-sets-train --diff-fingers-across-sets-val --diff-sensors-across-sets-val \
-        --scale-factor 1 --log-interval 100
-done
+        --scale-factor 2 --log-interval 100  
+    folders="${DEMOGRAPHICS_ROOT}/${MALE_GROUP} ${DEMOGRAPHICS_ROOT}/${FEMALE_GROUP}"
+    CUDA_VISIBLE_DEVICES=$2 python3 parameterized_runner.py \
+        --datasets "${folders}" \
+        --val-datasets "${folders}" \
+        --posttrained-model-path $1/model_weights/demographic_model_combined_gender.pth \
+        --temp_model_dir 'temp_weights' --results_dir "$1/results" \
+        --diff-fingers-across-sets-train --diff-sensors-across-sets-train --diff-fingers-across-sets-val --diff-sensors-across-sets-val \
+        --scale-factor 2 --log-interval 100      
 
-##############################
-### DEMOGRAPHIC FAIRNESS
-###############
+    #########
+    # Tests generalizability
+    #########################
 
-#########
-# Tests generalizability
-#########################
-
-### Race
-for train_group in $CAUCASIAN_DESCENT $NON_CAUCASIAN $COMBINED_RACE
-do
-    for test_group in $CAUCASIAN_DESCENT $NON_CAUCASIAN
+    ### Race
+    for train_group in $CAUCASIAN_DESCENT $NON_CAUCASIAN "combined_race"
     do
-        demographic_model=$1/model_weights/demographic_model_${train_group}.pth
-        demographic_folder="${DEMOGRAPHICS_ROOT}/${test_group}"
-        python3 parameterized_multiple_finger_tester.py \
-            --dataset $demographic_folder \
-            --weights $demographic_model \
-            --cuda "cuda:$2" \
-            --num_fingers 1 \
-            --output_root "$FAIRNESS_OUTPUT_FOLDER/sd302/train_${train_group}_test_${test_group}" \
-            --scale_factor 4 \
-            --diff_fingers_across_sets \
-            --diff_fingers_within_set \
-            --diff_sensors_across_sets \
-            --same_sensor_within_set
+        for test_group in $CAUCASIAN_DESCENT $NON_CAUCASIAN
+        do
+            demographic_model=$1/model_weights/demographic_model_${train_group}.pth
+            demographic_folder="${DEMOGRAPHICS_ROOT}/${test_group}"
+            python3 parameterized_multiple_finger_tester.py \
+                --dataset $demographic_folder \
+                --weights $demographic_model \
+                --cuda "cuda:$2" \
+                --num_fingers 1 \
+                --output_root "$FAIRNESS_OUTPUT_FOLDER/sd302/train_${train_group}_test_${test_group}" \
+                --scale_factor 4 \
+                --diff_fingers_across_sets \
+                --diff_fingers_within_set \
+                --diff_sensors_across_sets \
+                --same_sensor_within_set
+        done
     done
-done
-### Gender
-for train_group in $MALE_GROUP $FEMALE_GROUP $COMBINED_GENDER
-do
-    for test_group in $MALE_GROUP $FEMALE_GROUP
+    ### Gender
+    for train_group in $MALE_GROUP $FEMALE_GROUP "combined_gender"
     do
-        demographic_model=$1/model_weights/demographic_model_${train_group}.pth
-        demographic_folder="${DEMOGRAPHICS_ROOT}/${test_group}"
-        python3 parameterized_multiple_finger_tester.py \
-            --dataset $demographic_folder \
-            --weights $demographic_model \
-            --cuda "cuda:$2" \
-            --num_fingers 1 \
-            --output_root "$FAIRNESS_OUTPUT_FOLDER/sd302/train_${train_group}_test_${test_group}" \
-            --scale_factor 4 \
-            --diff_fingers_across_sets \
-            --diff_fingers_within_set \
-            --diff_sensors_across_sets \
-            --same_sensor_within_set
+        for test_group in $MALE_GROUP $FEMALE_GROUP
+        do
+            demographic_model=$1/model_weights/demographic_model_${train_group}.pth
+            demographic_folder="${DEMOGRAPHICS_ROOT}/${test_group}"
+            python3 parameterized_multiple_finger_tester.py \
+                --dataset $demographic_folder \
+                --weights $demographic_model \
+                --cuda "cuda:$2" \
+                --num_fingers 1 \
+                --output_root "$FAIRNESS_OUTPUT_FOLDER/sd302/train_${train_group}_test_${test_group}" \
+                --scale_factor 4 \
+                --diff_fingers_across_sets \
+                --diff_fingers_within_set \
+                --diff_sensors_across_sets \
+                --same_sensor_within_set
+        done
     done
 done
 
