d747f894ce64792ccc357b738b03c160f660911c
gabeguo <gzg2104@columbia.edu>
2023-07-29 19:38:31 -0400

    wrote code to show demographic results - it looks the way I want it now

--- stats_and_graphs/plot_demographic_results.py
+++ stats_and_graphs/plot_demographic_results.py
@@ -8,13 +8,21 @@ import numpy as np
 
 RACIAL_GROUPS = ['white', 'non-white', 'race']
 GENDER_GROUPS = ['female', 'male', 'gender']
-CURR_GROUPS = GENDER_GROUPS #RACIAL_GROUPS
+POSSIBLE_GROUPINGS = {'Race': RACIAL_GROUPS, 'Gender': GENDER_GROUPS}
 
 def get_group_name(filepath, is_weights_path):
     name = filepath.split('/')[-1]
     if is_weights_path:
         name = name[len('demographic_model_'):-4]
     return name
+
+def capitalize(token):
+    if 'sd' in token:
+        return f'SD{token[2:]}'
+    elif '-' in token:
+        return '-'.join([subtoken.capitalize() for subtoken in token.split('-')])
+    else:
+        return token.capitalize()
 
 # Get a list of all subdirectories
 subdirs = glob.glob('/home/gabeguo/fingerprint_results/paper_results/fairness/sd302/*', recursive=False)
@@ -52,37 +60,44 @@ for subdir in subdirs:
 
     train_group = get_group_name(list(weights)[0], is_weights_path=True)
     test_group = get_group_name(list(datasets)[0], is_weights_path=False)
-    if any([x in train_group for x in CURR_GROUPS]) and any([x in test_group for x in CURR_GROUPS]):
-        all_train_groups.add(train_group)
-        all_test_groups.add(test_group)
+    #if any([x in train_group for x in CURR_GROUPS]) and any([x in test_group for x in CURR_GROUPS]):
+    all_train_groups.add(train_group)
+    all_test_groups.add(test_group)
 
     # Compute mean and standard deviation of the ROC AUC scores
     mean_score = np.mean(scores)
-    std_score = np.std(scores)
+    std_score = np.std(scores, ddof=1)
 
     roc_auc_scores[(train_group, test_group)] = {'mean': mean_score, 'std': std_score}
 
-all_train_groups = list(all_train_groups)
-all_test_groups = list(all_test_groups)
+for curr_grouping_name in POSSIBLE_GROUPINGS:
+    curr_grouping = POSSIBLE_GROUPINGS[curr_grouping_name]
+    curr_train_groups = [x for x in all_train_groups if any(y in x for y in curr_grouping)]
+    curr_test_groups = [x for x in all_test_groups if any(y in x for y in curr_grouping)]
+    curr_train_groups.sort()
+    curr_test_groups.sort(); curr_test_groups.reverse()
+    print(curr_train_groups, curr_test_groups)
 
-mean_roc_auc = np.zeros((len(all_train_groups), len(all_test_groups)))
-std_roc_auc = np.zeros((len(all_train_groups), len(all_test_groups)))
+    mean_roc_auc = np.zeros((len(curr_train_groups), len(curr_test_groups)))
+    std_roc_auc = np.zeros((len(curr_train_groups), len(curr_test_groups)))
 
-#print('\n'.join([str(item) for item in roc_auc_scores]))
+    #print('\n'.join([str(item) for item in roc_auc_scores]))
 
-for train_idx, train_group in enumerate(all_train_groups):
-    for test_idx, test_group in enumerate(all_test_groups):
-        mean_roc_auc[train_idx, test_idx] = roc_auc_scores[(train_group, test_group)]['mean']
-        std_roc_auc[train_idx, test_idx] = roc_auc_scores[(train_group, test_group)]['std']
+    for train_idx, train_group in enumerate(curr_train_groups):
+        for test_idx, test_group in enumerate(curr_test_groups):
+            mean_roc_auc[train_idx, test_idx] = roc_auc_scores[(train_group, test_group)]['mean']
+            std_roc_auc[train_idx, test_idx] = roc_auc_scores[(train_group, test_group)]['std']
 
-# Create a heatmap with mean values, but annotations for both mean and std
-plt.figure(figsize=(10, 8))
-all_test_groups = [' '.join([token.capitalize() for token in name.split('_')]) for name in all_test_groups]
-all_train_groups = [' '.join([token.capitalize() for token in name.split('_')]) for name in all_train_groups]
-annot = [[f"{mean:.3f} ± {std:.3f}" for mean, std in zip(mean_row, std_row)] 
-        for mean_row, std_row in zip(mean_roc_auc, std_roc_auc)]
-sns.heatmap(mean_roc_auc, annot=annot, 
-            fmt='', annot_kws={'fontsize':15}, cmap='coolwarm',
-            xticklabels=all_test_groups, yticklabels=all_train_groups)
-plt.title('Mean ROC AUC Scores with Standard Deviations')
-plt.show()
+    # Create a heatmap with mean values, but annotations for both mean and std
+    plt.figure(figsize=(8, 12))
+    curr_test_groups = [' '.join([capitalize(token) for token in name.split('_')]) for name in curr_test_groups]
+    curr_train_groups = [' '.join([capitalize(token) for token in name.split('_')]) for name in curr_train_groups]
+    annot = [[f"{mean:.3f} ± {std:.3f}" for mean, std in zip(mean_row, std_row)] 
+            for mean_row, std_row in zip(mean_roc_auc, std_roc_auc)]
+    heatmap = sns.heatmap(mean_roc_auc, annot=annot, 
+                fmt='', annot_kws={'fontsize':15}, cmap='coolwarm',
+                xticklabels=curr_test_groups, yticklabels=curr_train_groups)
+    heatmap.set_xlabel('Testing Group', fontsize=14)
+    heatmap.set_ylabel('Training Group', fontsize=14)
+    plt.title(f'ROC AUC (Mean ± Std Err): {curr_grouping_name} Demographics')
+    plt.show()
