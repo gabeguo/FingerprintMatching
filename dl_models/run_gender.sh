35092e3e96010c873514c8ab9dba5a6e3764e15b
gabeguo <gzg2104@columbia.edu>
2023-09-11 12:11:19 -0400

    increasing early stopping interval to 105 in run_gender and run_race, due to one of the models only starting to converge at epoch 85 - avoid this in future

--- dl_models/run_gender.sh
+++ dl_models/run_gender.sh
@@ -34,7 +34,8 @@ do
             --posttrained-model-path $1/model_weights/demographic_model_${category}_${i}.pth \
             --temp_model_dir 'temp_weights' --results_dir "$1/results" \
             --diff-fingers-across-sets-train --diff-sensors-across-sets-train --diff-fingers-across-sets-val --diff-sensors-across-sets-val \
-            --scale-factor 2 --log-interval 100
+            --scale-factor 2 --log-interval 100 \
+            --early_stopping_interval 105
     done
     # Also train combined demographics
     folders="${DEMOGRAPHICS_ROOT}/${MALE_GROUP} ${DEMOGRAPHICS_ROOT}/${FEMALE_GROUP}"
@@ -44,7 +45,8 @@ do
         --posttrained-model-path $1/model_weights/demographic_model_combined_gender_${i}.pth \
         --temp_model_dir 'temp_weights' --results_dir "$1/results" \
         --diff-fingers-across-sets-train --diff-sensors-across-sets-train --diff-fingers-across-sets-val --diff-sensors-across-sets-val \
-        --scale-factor 2 --log-interval 100 
+        --scale-factor 2 --log-interval 100 \
+        --early_stopping_interval 105 
 
     ### Test generalizability
     for train_group in $MALE_GROUP $FEMALE_GROUP "combined_gender"
--- dl_models/run_race.sh
+++ dl_models/run_race.sh
@@ -34,7 +34,8 @@ do
             --posttrained-model-path $1/model_weights/demographic_model_${category}_${i}.pth \
             --temp_model_dir 'temp_weights' --results_dir "$1/results" \
             --diff-fingers-across-sets-train --diff-sensors-across-sets-train --diff-fingers-across-sets-val --diff-sensors-across-sets-val \
-            --scale-factor 2 --log-interval 100
+            --scale-factor 2 --log-interval 100 \
+            --early_stopping_interval 105
     done
     # Also train combined demographics
     folders="${DEMOGRAPHICS_ROOT}/${CAUCASIAN_DESCENT} ${DEMOGRAPHICS_ROOT}/${NON_CAUCASIAN}"
@@ -44,7 +45,8 @@ do
         --posttrained-model-path $1/model_weights/demographic_model_combined_race_${i}.pth \
         --temp_model_dir 'temp_weights' --results_dir "$1/results" \
         --diff-fingers-across-sets-train --diff-sensors-across-sets-train --diff-fingers-across-sets-val --diff-sensors-across-sets-val \
-        --scale-factor 2 --log-interval 100
+        --scale-factor 2 --log-interval 100 \
+        --early_stopping_interval 105
 
     ### Test
     for train_group in $CAUCASIAN_DESCENT $NON_CAUCASIAN "combined_race"
